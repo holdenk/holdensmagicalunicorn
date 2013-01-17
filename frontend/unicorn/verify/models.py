bdc6a22f339d310ba47a5bad40fbb9f5ecb7caf8
Holden Karau <holden@pigscanfly.ca>
2013-01-16 21:28:58 -0800

    Start writing the model

--- frontend/unicorn/verify/models.py
+++ frontend/unicorn/verify/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class PatchInfo(models.Model):
+    diff_url=models.CharField(max_length=1000, help_text="The URL of the github diff")
+    target_username=models.CharField(max_length=1000, help_text="The target github user to apply the pull request to (if approved)")
+    message_txt=models.CharField(max_length=1000, help_text="The commit message text")
+    reviewer_username=models.CharField(max_length=1000, help_text="The username of the reviewer")
+    good = models.BooleanField(max_length=1000, help_text="Is this patch good",
+                               default=False)
+    examined = models.BooleanField(max_length=1000, help_text="Has this patch been examined",
+                                   default=False)
+    exported = models.BooleanField(max_length=1000, help_text="Has this patch been examined",
+                                   default=False)
+
