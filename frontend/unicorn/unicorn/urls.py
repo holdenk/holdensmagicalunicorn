f2087f9f3d6ccecd2fb996056ffd457f53d12aa4
Holden Karau <holden@pigscanfly.ca>
2013-01-18 22:36:00 -0800

    Add some stuff

--- frontend/unicorn/unicorn/urls.py
+++ frontend/unicorn/unicorn/urls.py
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
-     url(r'^/', include('unicorn.verify.urls')),
+     url(r'^$', 'verify.views.index'),
 )
--- frontend/unicorn/verify/views.py
+++ frontend/unicorn/verify/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def index(request):
+    t = loader.get_template("index.html")
+    c = Context()
+    return HttpResponse(t.render(c))
