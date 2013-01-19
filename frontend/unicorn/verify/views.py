97803c9f71cb7d44437d32a2c391937aff73858f
Holden Karau <holden@pigscanfly.ca>
2013-01-18 22:43:15 -0800

    Make a review view login required

--- frontend/unicorn/verify/views.py
+++ frontend/unicorn/verify/views.py
@@ -1,8 +1,15 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.contrib.auth.decorators import login_required 
 
 def index(request):
     t = loader.get_template("index.html")
+    c = Context()
+    return HttpResponse(t.render(c))
+
+@login_required
+def review(request):
+    t = loader.get_template("review.html")
     c = Context()
     return HttpResponse(t.render(c))
