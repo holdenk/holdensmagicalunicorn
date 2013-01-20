70577178ca261c111f5934324a9d64c2f3c255ff
Holden Karau <holden@pigscanfly.ca>
2013-01-19 20:27:09 -0800

    Get the username

--- frontend/unicorn/verify/views.py
+++ frontend/unicorn/verify/views.py
@@ -2,6 +2,8 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.contrib.auth.decorators import login_required 
+from models import PatchInfo
+from social_auth.models import *
 
 def index(request):
     t = loader.get_template("index.html")
@@ -11,5 +13,7 @@ def index(request):
 @login_required
 def review(request):
     t = loader.get_template("review.html")
-    c = Context()
+    social = UserSocialAuth.objects.filter(user = request.user)
+    githubusername = social[0].user
+    c = Context({'ghusername' : githubusername})
     return HttpResponse(t.render(c))
