928fde8903a4625287d9208eb7aa19cbe01c8ba5
Holden Karau <holden@pigscanfly.ca>
2013-09-15 15:49:33 -0700

    Fix some of the perl shell REs

--- backend/Unicorn/Shell.pm
+++ backend/Unicorn/Shell.pm
@@ -15,12 +15,12 @@ sub fix_shell {
         #Probably!
         #Handle with [-e foo -e bar]
 	# http://www.pixelbeat.org/programming/shell_script_mistakes.html
-        $rt =~ s/^\s*if\s*\[\s*\-e\s*(\w+?)\s*\-e\s*(\w+?)\s*\]\s*$/if [-e $1] || [-e $2]/;
+        $rt =~ s/^\s*if\s*\[\s*\-e\s*(\w+?)\s*\-e\s*(\w+?)\s*\]\s*/if [-e $1] || [-e $2]/;
         #Double negative
-        $rt =~ s/^\s*if\s*\[\s*\!\-z\s*(\"\$\w*\")\]\s*$/if [$1]/;
+        $rt =~ s/^\s*if\s+\[\s*\!\-z\s*(\"\$\w+\")\]\s*/if [$1]/;
         #Check for cat pipe to grep
 	#i.e cat foo | grep baz
-        $rt =~ s/^\s*cat\s*(\w+)\s*\|\s*grep\s+([\w\"\\]+)\s*$/grep $2 < $1/
+        $rt =~ s/^\s*cat\s*(\w+)\s*\|\s*grep\s+([\w\"\\]+)\s*/grep $2 < $1/
     } else {
         return 0;
     }
@@ -38,7 +38,7 @@ sub check_shell {
             return 1;
         }
         #Double negative
-        if ($rt =~ /\[\s*\!\-z\"\$\w*\"\]/) {
+        if ($rt =~ /\[\s*\!\-z\"\$\w+\"\]/) {
             return 1;
         }
         #Check for cat pipe to grep
